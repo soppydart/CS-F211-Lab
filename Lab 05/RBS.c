dfab96e928f11053fa0ddcee79fc064e83f1f49a
Pritam <pritambasu14@gmail.com>
2023-02-27 02:46:14 +0530

    Added problem A

---
+++ Lab 05/RBS.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void solve(char *arr, int left, int right, int i)
+{
+    if (left == 0 && right == 0)
+    {
+        arr[i] = '\0';
+        printf("%s\n", arr);
+    }
+    if (left > 0)
+    {
+        arr[i] = '(';
+        solve(arr, left - 1, right, i + 1);
+    }
+    if (right > left)
+    {
+        arr[i] = ')';
+        solve(arr, left, right - 1, i + 1);
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char *arr = malloc((n) * sizeof(char));
+    int left, right;
+    left = right = n / 2;
+    solve(arr, left, right, 0);
+
+    return 0;
+}
